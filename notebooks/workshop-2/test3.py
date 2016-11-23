9be9af396f50c51c18d1ccc762a9d052942e3dfb
danilnagy <kidvsking@yahoo.com>
2016-11-23 13:50:52 -0500

    update ws-2

--- notebooks/workshop-2/test3.py
+++
@@ -1,6 +0,0 @@
-lst = ['cylinder', 'cylinder', 'cylinder', 'cylinder', 'cylinder', 'cylinder', 'cylinder', 'cylinder', 'cone', 'cube', 'cylinder']
-
-pred = max(set(lst), key=lst.count)
-
-print pred
-print '{:.2%}'.format(float(lst.count(pred)) / len(lst))
